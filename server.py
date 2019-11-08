96d645ab4c241001cf746c3bcd89f3ad31dc6075
Asocia <akkayas17@itu.edu.tr>
2019-11-09 01:26:59 +0300

    Configure the app and attach view functions to endpoints

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask
+
+import views
 
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("settings")
+    app.add_url_rule("/", view_func=views.home_page)
+    app.add_url_rule("/search_movie", view_func=views.search_movie_page)
+    app.add_url_rule("/discover", view_func=views.discover_page)
+    app.add_url_rule("/notifications", view_func=views.notifications_page)
+    app.add_url_rule("/profile", view_func=views.profile_page)
 
-
-@app.route("/")
-def home_page():
-    return 'render_template("home.html")'
+    return app
 
 
 if __name__ == "__main__":
-    app.run()
+    app = create_app()
+    app.run(host="0.0.0.0")
