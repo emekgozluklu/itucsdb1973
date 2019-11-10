8e798266b0a919a963765b53d8c4065810bfd305
Asocia <akkayas17@itu.edu.tr>
2019-11-11 02:49:47 +0300

    Get db url from environment variables

--- server.py
+++ server.py
@@ -6,14 +6,17 @@ import dbinit
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("settings")
+    if app.env == 'production':
+        app.config.from_object("settings.ProductionConfig")
+    else:
+        app.config.from_object("settings.DevelopmentConfig")
     app.add_url_rule("/", view_func=views.home_page)
     app.add_url_rule("/search_movie", view_func=views.search_movie_page)
     app.add_url_rule("/discover", view_func=views.discover_page)
     app.add_url_rule("/notifications", view_func=views.notifications_page)
     app.add_url_rule("/profile", view_func=views.profile_page)
     app.add_url_rule("/movie/<int:movie_key>", view_func=views.movie_page)
-    db = DBHelper("postgres://postgres:docker@localhost:5432/postgres")
+    db = DBHelper(app.config["DATABASE_URL"])
     dbinit.initialize(db.db_url)
     app.config["db"] = db
     return app
--- settings.py
+++ settings.py
@@ -1,2 +1,17 @@
-port = 8080
-debug = True
+import os
+
+
+class Config:
+    port = 8080
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    DATABASE_URL = "postgres://postgres:docker@localhost:5432/postgres"
+    SECRET_KEY = ""
