dfe57406967db41b2e32bb0abe184b4912fc72b6
Asocia <akkayas17@itu.edu.tr>
2019-11-13 01:14:13 +0300

    Implement delete functionality for movies

--- server.py
+++ server.py
@@ -6,10 +6,11 @@ import dbinit
 
 def create_app():
     app = Flask(__name__)
+    GP = ["GET", "POST"]
     app.config.from_object("settings.Config")
     app.add_url_rule("/", view_func=views.home_page)
     app.add_url_rule("/search_movie", view_func=views.search_movie_page)
-    app.add_url_rule("/discover", view_func=views.discover_page)
+    app.add_url_rule("/discover", view_func=views.discover_page, methods=GP)
     app.add_url_rule("/notifications", view_func=views.notifications_page)
     app.add_url_rule("/profile", view_func=views.profile_page)
     app.add_url_rule("/movie/<int:movie_key>", view_func=views.movie_page)
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import render_template, current_app
+from flask import render_template, current_app, request, redirect, url_for
 from itucsdb1973.data_model import Movie
 
 
@@ -12,8 +12,14 @@ def search_movie_page():
 
 def discover_page():
     db = current_app.config["db"]
-    movies = db.get_items(Movie, ("title", "release_date"))
-    return render_template("movies.html", movies=movies)
+    if request.method == "GET":
+        movies = db.get_items(Movie, ("title", "release_date"))
+        return render_template("movies.html", movies=movies)
+    else:
+        form_movie_keys = request.form.getlist("movie_keys")
+        for form_movie_key in form_movie_keys:
+            db.delete_items(Movie, id=form_movie_key)
+        return redirect(url_for("discover_page"))
 
 
 def movie_page():
