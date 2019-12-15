e54953a629e4f480a187d8014f7ca735c74f48f5
Asocia <akkayas17@itu.edu.tr>
2019-12-15 22:43:46 +0300

    Restrict length of user name and password fields

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import Length
 
 
 class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    username = StringField("Username", validators=[Length(min=4, max=25)])
+    password = PasswordField("Password", validators=[Length(min=6, max=35)])
