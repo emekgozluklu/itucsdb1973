080af92555a7120efca30148169268d9945646e3
Asocia <akkayas17@itu.edu.tr>
2019-12-15 14:30:54 +0300

    Follow PEP8 rules

--- server.py
+++ server.py
@@ -6,17 +6,17 @@ import dbinit
 
 def create_app():
     app = Flask(__name__)
-    GP = ["GET", "POST"]
+    gp = ["GET", "POST"]
     app.config.from_object("settings.Config")
     app.add_url_rule("/", view_func=views.home)
     app.add_url_rule("/search_movie", view_func=views.search_movie)
-    app.add_url_rule("/discover", view_func=views.discover, methods=GP)
+    app.add_url_rule("/discover", view_func=views.discover, methods=gp)
     app.add_url_rule("/notifications", view_func=views.notifications)
     app.add_url_rule("/profile", view_func=views.profile)
     app.add_url_rule("/movie/<int:movie_key>", view_func=views.movie)
-    app.add_url_rule("/add_movie", view_func=views.add_movie, methods=GP)
+    app.add_url_rule("/add_movie", view_func=views.add_movie, methods=gp)
     app.add_url_rule("/add/<string:item>",
-                     view_func=views.add_single_field_item, methods=GP)
+                     view_func=views.add_single_field_item, methods=gp)
     db_url = app.config["DATABASE_URL"]
     db = DBClient(db_url)
     dbinit.initialize(db_url)
