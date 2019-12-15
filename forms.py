b84571e7f75929b0434ae7a66e5c0fe0e547e0c0
Asocia <akkayas17@itu.edu.tr>
2019-12-15 22:49:05 +0300

    Create register form

--- forms.py
+++ forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import Length
+from wtforms import StringField, PasswordField, FileField
+from wtforms.validators import Length, EqualTo
 
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[Length(min=4, max=25)])
     password = PasswordField("Password", validators=[Length(min=6, max=35)])
+
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[Length(min=4, max=25)])
+    email = StringField("Username", validators=[Length(min=6, max=35)])
+    password = PasswordField('New Password', [Length(min=4, max=35),
+                                              EqualTo('confirm',
+                                                      message='Passwords must match')
+                                              ])
+    confirm = PasswordField('Repeat Password')
+    profile_photo = FileField("Select a file...")
